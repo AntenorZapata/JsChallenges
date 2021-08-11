198e24abb12d6cad35b62149c84764a372e3c1b6
Antenor <antenorzapata@gmail.com>
2021-08-11 08:43:02 -0300

    linked list push and pop methods

--- THEORY/day1.js
+++ THEORY/day1.js
@@ -29,8 +29,62 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let temp = this.head;
+    let pre = this.head;
+
+    for (let i = 0; i < this.length; i += 1) {
+      if (temp.next) {
+        pre = temp;
+        temp = temp.next;
+      }
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
 }
 
-let myLinkedList = new LinkedList(4);
+let myLinkedList = new LinkedList(2);
+
+myLinkedList.push(9);
+myLinkedList.push(11);
+myLinkedList.pop();
+
+// for (const a in myLinkedList) {
+//   console.log(a);
+// }
 
 console.log(myLinkedList);
+
+// head: {
+// 	value 2,
+// 	next: {
+// 		value: 9,
+// 		next: {
+// 			value: 12,
+// 			next: null
+// 		}
+// 	}
+// }
