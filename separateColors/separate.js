a6a8f8e3b908ba484bcbc1c171a6ee49e333f52a
Antenor <antenorzapata@gmail.com>
2021-08-13 18:11:59 -0300

    100%

--- separateColors/separate.js
+++ separateColors/separate.js
@@ -1,8 +1,8 @@
-// Ow, rapaz, eu sei que é óbvio, a gente sempre soube que arrays são objetos e tal, mas eu nunca tinha parado pra pensar que a gente consegue acessá-los exatamente como os objetos.
-// 10:40
-// Tipo, definindo novas chaves, arr['props'] = 1; OU arr.props2 = 'value2';
-// 10:40
-// mesmo o length, a gente pode acessar com colchetes, tipo arr[‘length’].
+// // Ow, rapaz, eu sei que é óbvio, a gente sempre soube que arrays são objetos e tal, mas eu nunca tinha parado pra pensar que a gente consegue acessá-los exatamente como os objetos.
+// // 10:40
+// // Tipo, definindo novas chaves, arr['props'] = 1; OU arr.props2 = 'value2';
+// // 10:40
+// // mesmo o length, a gente pode acessar com colchetes, tipo arr[‘length’].
 
 const wrongData = [
   'preto-PP',
@@ -21,12 +21,11 @@ const wrongData = [
 
 const convert = (dataArr) => {
   const obj = {};
-  const obj2 = {};
-  const obj3 = {};
   const values = [];
   const keys = [];
   const occurrences = {};
-  let allValues = [];
+  let count = 0;
+  let count2 = 0;
 
   // crio arrays separados para keys and values
   wrongData.map((item) =>
@@ -37,73 +36,34 @@ const convert = (dataArr) => {
   );
 
   // descubro o número de ocrrencias de cada cor
-
   for (var i = 0, j = keys.length; i < j; i++) {
     occurrences[keys[i]] = (occurrences[keys[i]] || 0) + 1;
   }
+
+  // console.log(values);
 
   // crio um objeto com cada cor
   for (const item of wrongData) {
     // [preto, pp]
     item.split('-').reduce((acc, curr, index) => {
       if (obj[acc]) {
-        obj[acc] = { ...obj[acc], [curr]: 1 };
+        obj[acc] = { ...obj[acc], [curr]: 0 };
       } else {
-        obj[acc] = { [curr]: 1 };
+        obj[acc] = { [curr]: 0 };
       }
     });
   }
 
-  // for (const item of wrongData) {
-  //   // [preto, pp]
-  //   item.split('-').reduce((acc, curr, index) => {
-  //     if (Object.keys(obj[acc]).includes(curr)) {
-  //       Object.keys(obj[acc])[index] = +1;
-  //     }
-  //   });
-  // }
-
-  // for (const key in obj) {
-  //   values.forEach((size) => {
-  //     if (obj[key][size]) {
-  //       obj[key][size] += 1;
-  //     }
-  //   });
-  // }
+  for (const key in obj) {
+    while (count < occurrences[key]) {
+      obj[key][values[count2]] += 1;
+      count += 1;
+      count2 += 1;
+    }
+    count = 0;
+  }
 
   return obj;
 };
 
 console.log(convert(wrongData));
-// for (const item of wrongData) {
-//   // [preto, pp]
-//   item.split('-').reduce((acc, curr) => {
-//     obj[acc] = values.reduce((total, item, index) => {
-//       if (!total[item]) {
-//         total[item] = 1;
-//       } else {
-//         total[item] += 1;
-//       }
-//       return total;
-//     }, {});
-//   });
-// }
-
-// const black = values.slice(0, 5);
-// const white = values.slice(5, 7);
-// const red = values.slice(7, 8);
-// const blue = values.slice(8);
-
-// function splitArrayIntoChunksOfLen(arr, len) {
-//   var chunks = [],
-//     i = 0,
-//     n = arr.length;
-//   while (i < n) {
-//     chunks.push(arr.slice(i, (i += len)));
-//   }
-//   return chunks;
-// }
-// var alphabet = ['a', 'b', 'c', 'd', 'e', 'f'];
-// var alphabetPairs = splitArrayIntoChunksOfLen(alphabet, 2);
-
-// console.log(alphabetPairs);
