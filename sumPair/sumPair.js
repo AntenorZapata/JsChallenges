ab2093fcaa04de6c1d5199017e07f61814a2b565
Antenor <antenorzapata@gmail.com>
2021-12-03 20:33:16 -0300

    feat: sumPair with binary search

--- sumPair/sumPair.js
+++ sumPair/sumPair.js
@@ -1,26 +1,41 @@
 const arr = [3, 5, -4, 8, 11, 1, -1, 6];
 
 const sumPair = (arr, num) => {
-  const result = [];
-  const obj = { 1: "sim" };
-  for (const i of arr) {
-    for (const j of arr) {
-      if (i !== j && i + j === num) {
-        if (!result.includes(i) && !result.includes(j)) {
-          result.push(i, j);
-        }
-      }
-    }
-  }
+  // const result = [];
+  // const obj = { 1: "sim" };
+  // for (const i of arr) {
+  //   for (const j of arr) {
+  //     if (i !== j && i + j === num) {
+  //       if (!result.includes(i) && !result.includes(j)) {
+  //         result.push(i, j);
+  //       }
+  //     }
+  //   }
+  // }
 
-  for (const i of arr) {
-    if (num - i in obj) {
-      console.log(obj);
-      return [num - i, i];
+  // for (const i of arr) {
+  //   if (num - i in obj) {
+  //     return [num - i, i];
+  //   }
+  //   obj[i] = true;
+  // }
+  // return [];
+
+  arr.sort((a, b) => a - b);
+
+  let countLeft = 0;
+  let countRight = arr.length - 1;
+  let conditions = false;
+  while (!conditions) {
+    if (arr[countLeft] + arr[countRight] < num) {
+      countLeft += 1;
+    } else if (arr[countLeft] + arr[countRight > num]) {
+      countRight -= 1;
+    } else {
+      conditions = true;
+      return [arr[countLeft], arr[countRight]];
     }
-    obj[i] = true;
   }
-  return [];
 
   // return result;
 };
